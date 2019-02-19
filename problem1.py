2ec65084fba71d8cf591ced393f3093769752e64
D3str0yGM <47692331+D3str0yGM@users.noreply.github.com>
2019-02-19 21:17:33 +0400

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -1,1 +1,6 @@
-
+sayi = int(input("reqemi daxil edin: "))
+a  = sayi //1000
+b = (sayi - a*1000)//100
+c = (sayi - a*1000 - b*100)//10
+d = sayi - a*1000 - b*100 - c*10
+print("Reqemlerin toplami: ", a+b+c+d)
